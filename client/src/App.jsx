b05724552e111b2c960cae25b7d95eafbe5dc579
Armstrong104 <ameertalha22@gmail.com>
2025-03-14 03:43:57 +0600

    Add client
    - MUI Datagrid
    - useCallback
    - useMemo
    - axios

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import './App.css';
+import { ProductTable } from './components';
+
+function App() {
+  return (
+    <Box>
+      <ProductTable />
+    </Box>
+  );
+}
+
+export default App;
